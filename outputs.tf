f6a099ff6129a6c6306864e3fa3d213fdbb17bc6
Richard G <guo_yiwei@yahoo.com>
2019-10-15 13:30:53 -0700

    switch output to 0.12 syntax, added name

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "subnet" {
   description = "The first subnet of this vpc"
-  value = "${aws_subnet.az.0.id}" 
+  value = "aws_subnet.az[0].id" 
 }
 
-#output "security_group_ids" {
-#  value =  ["${aws_security_group.allow_vpn.id}"]
-#}
-#
+output "name" {
+  description = "The name of this vpc"
+  value ="Terraform-var.client-var.environment"
+}
